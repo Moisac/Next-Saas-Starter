55cf9fa5c615243e014b4132e20f7e97e543b37c
moisac <moisaclaudiu2309@gmail.com>
2024-03-17 21:48:26 +0200

    remove auth layout

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -13,15 +13,6 @@ export const metadata = {
 export default function RegisterPage() {
   return (
     <div className="container grid h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Link
-        href="/login"
-        className={cn(
-          buttonVariants({ variant: "ghost" }),
-          "absolute right-4 top-4 md:right-8 md:top-8"
-        )}
-      >
-        Login
-      </Link>
       <div className="hidden h-full bg-muted lg:block" />
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
